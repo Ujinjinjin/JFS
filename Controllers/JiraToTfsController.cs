daad9299fc097807a282df447b79f8a54c071d01
Kamil Galladjov <kagallad@mts.ru>
2018-10-23 13:24:41 +0300

    Initial project setup

---
+++ Controllers/JiraToTfsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JFS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JiraToTfsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public string Test()
+        {
+            return "Hello darkness my old friend, I've come to talk with you again";
+        }
+    }
+}
