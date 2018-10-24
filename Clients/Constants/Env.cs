fbb18e8cb85bcea20e5b64686b98f5995e343c0f
Kamil Galladjov <kagallad@mts.ru>
2018-10-24 12:40:25 +0300

    Trying to authenticate application

---
+++ Clients/Constants/Env.cs
@@ -0,0 +1,8 @@
+﻿namespace JFS.Clients.Constants
+{
+    public static class Env
+    {
+        public const string ACCESS_TOKEN = "AZURE_DEVOPS_ACCESS_TOKEN";
+        public const string URI = "AZURE_DEVOPS_URI";
+    }
+}
