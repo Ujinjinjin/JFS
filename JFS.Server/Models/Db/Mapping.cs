5d5ada644331a19c0a37163c43c303bcfb94138e
Kamil Galladjov <kagallad@mts.ru>
2018-10-26 16:45:20 +0300

    Added application db context
    also fixed and added some field names

--- JFS.Server/Models/Db/Mapping.cs
+++ JFS.Server/Models/Db/Mapping.cs
@@ -7,7 +7,7 @@
         public Profile Profile { get; set; }
         public int CommonFieldId { get; set; }
         public CommonField CommonField { get; set; }
-        public int TfsFileId { get; set; }
+        public int TfsFieldId { get; set; }
         public TfsField TfsField { get; set; }
         public int JiraFieldId { get; set; }
         public JiraField JiraField { get; set; }
