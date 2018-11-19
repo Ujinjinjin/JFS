bcdfc12fb43274ec6d6f791190f89428a39af3ed
Kamil <gallkam@outlook.com>
2018-11-19 19:15:58 +0300

    Clean up

--- JFS.Server/Controllers/JiraToTfsController.cs
+++ JFS.Server/Controllers/JiraToTfsController.cs
@@ -8,7 +8,6 @@ using JFS.Utils;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace JFS.Controllers
